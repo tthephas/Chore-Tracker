133049abcb719e7e3d5be285e0982b5a9ca00432
thepjos <josh.thephasdin@gmail.com>
2023-03-07 12:36:26 -0600

    available chore list done -index. Built with real tasks

--- main_app/models.py
+++ main_app/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Chore(models.Model):
     name = models.CharField(max_length=100)
     type = models.CharField(max_length=100)
-    description = models.TextField(max_length=250)
+    description = models.TextField(max_length=400)
     amount = models.IntegerField()
 
     # override the str method in models to print better
